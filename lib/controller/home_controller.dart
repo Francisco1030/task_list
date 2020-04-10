75ea09b051bf5ba104b062368a4390101fe46b8b
Francisco Viana <franciscoviana862@gmail.com>
2020-04-10 19:29:20 -0300

    added CheckboxListTile;

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,11 +1,23 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:list_task/model/task.dart';
 import 'package:path_provider/path_provider.dart';
 
 class HomeController {
 
-  List<Task> tasks = [];
+  final taskController = TextEditingController();
+
+  List<Task> tasks = [
+    Task(title: "Atividade ED", status: false),
+    Task(title: "Atividade DB", status: false)
+  ];
+
+  void add() {
+    tasks.add(Task(title: taskController.text, status: false));
+    taskController.text = "";
+    print(taskController.value.text);
+  }
 
   Future<File> getFile() async {
     final directory = await getApplicationDocumentsDirectory();
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:list_task/controller/home_controller.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
+  HomeController homeController = new HomeController();
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  void addTask() {
+     setState(() {
+       widget.homeController.add();
+    }); 
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: widget.homeController.taskController,
                       decoration: InputDecoration(
                           labelText: "Nova tarefa: ",
                           labelStyle: TextStyle(color: Colors.blueAccent)),
@@ -36,10 +47,25 @@ class _MyHomePageState extends State<MyHomePage> {
                     color: Colors.blueAccent,
                     child: Text("ADD"),
                     textColor: Colors.white,
-                    onPressed: () {},
+                    onPressed: addTask,
                   )
                 ],
               ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: widget.homeController.tasks.length,
+                  itemBuilder: (context, index) {
+                    return CheckboxListTile(
+                      title: Text(widget.homeController.tasks[index].title),
+                      value: widget.homeController.tasks[index].status,
+                      secondary: CircleAvatar(
+                        child: Icon(widget.homeController.tasks[index].status == true ?
+                        Icons.check : Icons.error),
+                        ),
+                    );
+                  }),
             )
           ],
         ),
